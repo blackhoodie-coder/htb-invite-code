d208245ed135df96c67bafaeec379d3e44ff6f3b
blackhoodie-coder <69415935+blackhoodie-coder@users.noreply.github.com>
2020-08-09 14:13:17 +0530

    Create codegen.py

---
+++ codegen.py
@@ -0,0 +1,11 @@
+import base64
+import subprocess
+subprocess.call("curl -XPOST https://www.hackthebox.eu/api/invite/generate", shell=True)
+print('\n')
+api = str(input("see the line =, copy it any paste here:\n==> "))
+print('\n')
+dec = str(base64.standard_b64decode(api))
+print("[+]==> " + dec.replace('b', ''))
+print('\n')
+print("The format of the code is AAAA-BBBB-CCCC-DDDD-EEEE paste it in webiste")
+print('\n')
